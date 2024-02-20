bf9b0b2f16b328f8daddd37ce55ae68973594cc0
ChrisItisdud <chris.reichel@gmx.net>
2024-02-20 16:24:57 +0100

    improved main method, hide some stuff behind cfg(test)

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,29 @@ use demo::{ray::DEFAULT_PROPAGATION_SPEED, scene::SceneData, scene_builder};
 const DEFAULT_NUMBER_OF_RAYS: u32 = 100000;
 
 fn main() {
-    std::env::set_var("RUST_BACKTRACE", "1");
+    // std::env::set_var("RUST_BACKTRACE", "1");
     let args: Vec<String> = std::env::args().collect();
-    assert!(
-        args.len() >= 3,
-        "Please provide at least a scene index and an input file!"
-    );
 
-    let input_fname = &args[2];
+    let mut input_fname: Option<&str> = None;
+    let mut scene_index: Option<u32> = None;
+    let mut number_of_rays: u32 = DEFAULT_NUMBER_OF_RAYS;
+    let mut scaling_factor: f64 = DEFAULT_NUMBER_OF_RAYS as f64;
+
+    for arg in args.iter().skip(1) {
+        let arg_split: Vec<&str> = arg.split('=').collect();
+        assert!(arg_split.len() == 2, "Unknown argument {arg}");
+        match arg_split[0] {
+            "--fname" => input_fname = Some(arg_split[1]),
+            "--scene" => scene_index = arg_split[1].parse::<u32>().ok(),
+            "--rays" => number_of_rays = arg_split[1].parse::<u32>().unwrap_or_else(|_| panic!("\"--rays\" needs to be passed a number!")),
+            "--scaling-factor" => scaling_factor = arg_split[1].parse::<f64>().unwrap_or_else(|_| panic!("\"--rays\" needs to be passed a number!")),
+            _ => panic!("Unknown argument {}", arg_split[0]),
+        };
+    }
+
+    let Some(input_fname) = input_fname else {
+        panic!("Please provide a file name using \"--fname=FILENAME\"!")
+    };
     let mut input_file = std::fs::File::open(std::path::Path::new(input_fname))
         .unwrap_or_else(|_| panic!("Input file couldn't be opened!"));
     let (header, input_data) = wav::read(&mut input_file)
@@ -25,35 +40,28 @@ fn main() {
         wav::BitDepth::Empty => panic!("Input file did not contain any data!"),
     };
 
-    let scene_index = args[1]
-        .parse::<u32>()
-        .unwrap_or_else(|_| panic!("Scene index must be a number!"));
+    let Some(scene_index) = scene_index else {
+        println!("Please provide a valid scene index using \"--scene=INDEX\"! The following scene indices are supported:");
+        print_supported_scenes();
+        panic!();
+    };
     let scene = match scene_index {
         0 => scene_builder::static_cube_scene(),
         1 => scene_builder::static_receiver_scene(),
         2 => scene_builder::approaching_receiver_scene(header.sampling_rate),
         _ => {
-            println!("Invalid scene index! The following scene indices are supported");
-            println!("\t0 - Static Cube");
-            println!("\t1 - Static Receiver");
-            println!("\t2 - Approaching Receiver");
+            println!("Invalid scene index! The following scene indices are supported:");
+            print_supported_scenes();
             panic!();
-        },
+        }
     };
     let scene_name = match scene_index {
         0 => "static cube",
         1 => "static receiver",
         2 => "approaching receiver",
-        _ => "error"
+        _ => "error",
     };
     println!("Selected scene #{scene_index}: \"{scene_name}\".");
-
-    let mut number_of_rays = DEFAULT_NUMBER_OF_RAYS;
-    if args.len() >= 4 {
-        if let Result::Ok(value) = args[3].parse::<u32>() {
-            number_of_rays = value;
-        }
-    }
 
     let scene_data = SceneData::<typenum::U10>::create_for_scene(scene);
     println!("Calculating and applying {input_sound_len} impulse responses with {number_of_rays} rays each, this will take a loooong while...");
@@ -63,13 +71,25 @@ fn main() {
         number_of_rays,
         DEFAULT_PROPAGATION_SPEED,
         header.sampling_rate as f64,
-        number_of_rays as f64 / 10f64,
+        1f64 / scaling_factor,
     );
     let elapsed = time_start.elapsed().as_secs();
-    println!("Finished calculation in {}:{}:{}", elapsed / 3600, (elapsed % 3600) / 60, elapsed % 60);
+    println!(
+        "Finished calculation in {}:{}:{}",
+        elapsed / 3600,
+        (elapsed % 3600) / 60,
+        elapsed % 60
+    );
 
     let mut output_file = std::fs::File::create(std::path::Path::new("result.wav"))
         .unwrap_or_else(|_| panic!("Output file couldn't be opened!"));
     wav::write(header, &result, &mut output_file)
         .unwrap_or_else(|_| panic!("Output file couldn't be written to!"));
+}
+
+/// Print out all supported scene indices.
+fn print_supported_scenes() {
+    println!("\t0 - Static Cube");
+    println!("\t1 - Static Receiver");
+    println!("\t2 - Approaching Receiver");
 }
--- src/test_utils.rs
+++ src/test_utils.rs
@@ -1,6 +1,8 @@
 use std::collections::HashSet;
 use std::hash::Hash;
+#[cfg(test)]
 use nalgebra::Vector3;
+#[cfg(test)]
 use approx::abs_diff_eq;
 
 /// Check that two unordered collections of items are equal.
@@ -14,10 +16,12 @@ pub fn unordered_eq_without_duplicates<T: Eq + Hash>(a: &[T], b: &[T]) -> bool {
 
 /// Assert that the given two unordered collections of items are equal.
 /// This ignores duplicates within the collections!
+#[cfg(test)]
 pub fn assert_unordered_eq_ignoring_duplicates<T: Eq + Hash>(a: &[T], b: &[T]) {
     assert!(unordered_eq_without_duplicates(a, b));
 }
 
+#[cfg(test)]
 pub fn vector_abs_diff_eq(a: Vector3<f64>, b: Vector3<f64>) -> bool {
     for i in 0..3 {
         if !(abs_diff_eq!(a[i], b[i], epsilon=0.000001)) {
@@ -27,6 +31,7 @@ pub fn vector_abs_diff_eq(a: Vector3<f64>, b: Vector3<f64>) -> bool {
     true
 }
 
+#[cfg(test)]
 pub fn assert_vector_abs_diff_eq(a: Vector3<f64>, b: Vector3<f64>) {
     assert!(vector_abs_diff_eq(a, b), "assertion `left == right` failed. left: {a:?}, right: {b:?}");
 }
